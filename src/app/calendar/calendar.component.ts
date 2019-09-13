b89e833ed5630f8130d7e810d1059e88b522bb07
Yenny Carolina Garavito Mora <yenny.garavito@avaldigitallabs.com>
2019-09-13 14:18:57 -0500

    :zap: creacion listas

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -14,6 +14,7 @@ export class CalendarComponent implements OnInit {
   @Input() day;
   @Input() format;
 
+  pickList: any[];
   yearList: any[];
   monthList: any[];
   dayList: any[];
@@ -21,6 +22,11 @@ export class CalendarComponent implements OnInit {
   formatDate: any;
   separator: string;
   inputDate: any;
+  pickYear: boolean;
+  pickMonth: boolean;
+  pickDay: boolean;
+  dayNameList: any[];
+  monthNames: any[];
 
   constructor() { }
 
@@ -29,13 +35,23 @@ export class CalendarComponent implements OnInit {
     this.day = this.day ? this.day : '01';
     this.year = this.year ? this.year : '2000';
     this.yearList = [];
+    this.monthList = [];
+    this.dayList = [];
     this.formatDate = [];
     this.labelYear = 'Seleccione el año';
     const today = new Date();
     this.orderFormatDate();
+    this.pickYear = false;
+    this.pickMonth = false;
+    this.pickDay = false;
+    this.dayNameList = ['L', 'M', 'M', 'J', 'V', 'S', 'D'];
+    this.monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio',
+      'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
 
     const paramDate = new Date(`${this.year}` + `${this.separator}` + `${this.month}` + `${this.separator}` + `${this.day}`);
     this.getYears(today, paramDate);
+    this.getMonths();
 
     this.inputDate = paramDate;
   }
@@ -59,7 +75,7 @@ export class CalendarComponent implements OnInit {
           {name: 'day', placeholder: 'DIA', required: 'required', readonly: '', tabIndex: '2'},
           {name: 'separator', placeholder: '/', required: '', readonly: 'readonly', tabIndex: '-1'},
           {name: 'month', placeholder: 'MES', required: 'required', readonly: '', tabIndex: '3'}
-        ]
+        ];
         this.separator = '/';
         break;
       case 'DD/MM/YYYY':
@@ -108,6 +124,25 @@ export class CalendarComponent implements OnInit {
     }
   }
 
+  daysMaxInMonth(month, year) {
+      return 32 - new Date(year, month, 32).getDate();
+  }
+
+  getMonths() {
+    for (let m = 1; m <= 12; m++) {
+      this.monthList.push(m + '-' + this.monthNames[m - 1]);
+    }
+  }
+
+  getMonthWithDays() {
+    this.dayList = [];
+    const maxDay = this.daysMaxInMonth(this.month, this.year);
+    const firstDay = (new Date(this.month, this.year)).getDay();
+    for (let d = 1; d <= maxDay + 1; d++) {
+      this.dayList.push(d < 10 ? '0' + d : d);
+    }
+  }
+
   searchDate(event) {
     let arrayEvent;
     let arrayEventSuggestions;
@@ -142,4 +177,26 @@ export class CalendarComponent implements OnInit {
     console.log(this.yearList);
   }
 
+  focusDisplay(eventF) {
+
+    if (eventF.target.id === 'year') {
+      this.pickList = this.yearList;
+      this.pickYear = true;
+      this.pickMonth = false;
+      this.pickDay = false;
+    } else {
+      if (eventF.target.id === 'month') {
+        this.pickList = this.monthList;
+        this.pickYear = false;
+        this.pickMonth = true;
+        this.pickDay = false;
+      } else {
+        this.getMonthWithDays();
+        this.pickList = this.dayList;
+        this.pickYear = false;
+        this.pickMonth = false;
+        this.pickDay = true;
+      }
+    }
+  }
 }
